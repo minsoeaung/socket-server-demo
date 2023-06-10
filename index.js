456aa104f74be3bc33e2176a6060b0f8177affe9
minsoeaung <minsoeaung2001@gmail.com>
2023-06-10 21:50:50 +0630

    Initial

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const {createServer} = require("http");
+const {Server} = require("socket.io");
+const fs = require('fs');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: "*"
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id, 'connected');
+
+  setInterval(() => {
+    socket.emit('every_5_s', 'hello from server');
+  }, 5000)
+
+  socket.on('text_message', (text) => {
+    console.log('TEXT', text);
+  })
+
+  socket.on('json_message', (json) => {
+    console.log('JSON', json);
+  })
+
+  socket.on('file_message', (file) => {
+    const data = file.replace(/^data:image\/\w+;base64,/, "");
+    const buf = Buffer.from(data, 'base64');
+    fs.writeFile(__dirname + `/files/${Math.random()}.png`, buf, (error) => {
+      if (error) {
+        console.log('ERROR', error);
+      } else {
+        console.log('File saved')
+      }
+    });
+  })
+});
+
+httpServer.listen(3000);
